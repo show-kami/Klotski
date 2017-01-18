dddc4b8c61a513246db1d1b1952fafb3ddba7763
Show.KAMI from SourceTree <mhasegawa.net@gmail.com>
2017-01-18 20:33:47 +0900

    [add] Board構造体を初期化する関数を作成

--- klotski.c
+++ klotski.c
@@ -3,14 +3,14 @@
 
 typedef struct board{
 	int NumOfMoves;
-	int state[5][4];
+	int state[4][5];
 	struct board *Parent;
 	struct board *FirstChild;
 	struct board *NextBrother;
 } board;
 
 void printBoard(int pstate[][5]);
-// void initializeStructBoard(int NumOfMoves, char state[][4], board *Parent, board *FirstChild, board *NextBrother);
+void initializeStructBoard(board *pb, int NumOfMoves, int state[][5], board *Parent, board *FirstChild, board *NextBrother);
 
 int main(void){
 	int InitialState[4][5] = {
@@ -19,11 +19,10 @@ int main(void){
 		{5, 5, 6, 7, 0},
 		{8, 8, 9, 7, 10}
 	};
-	// board *initial = malloc(sizeof(board));
+	board *pinitial = malloc(sizeof(board));
 
-	// initializeStructBoard(initial, 0, InitialState, NULL, NULL, NULL);
+	initializeStructBoard(pinitial, 0, InitialState, NULL, NULL, NULL);
 
-	printBoard(InitialState);
 	return 0;
 }
 
@@ -38,11 +37,16 @@ void printBoard(int pstate[][5]){
 	printf("--------------------\n");
 }
 
-// void initializeStructBoard(board *pb, int NumOfMoves, char state[][4], board *Parent, board *FirstChild, board *NextBrother){
-// 	int xi,yi;
-// 	for(yi = 0; yi < 5; yi)
-// 	pb->NumOfMoves = pb;
-// 	pb->Parent = Parent;
-// 	pb->FirstChild = FirstChild;
-// 	pb->NextBrother = NextBrother;
-// }
+void initializeStructBoard(board *pb, int NumOfMoves, int state[][5], board *Parent, board *FirstChild, board *NextBrother){
+	int xi,yi;
+	for(yi = 0; yi < 5; yi++){
+		for(xi = 0; xi < 4; xi++){
+			pb->state[xi][yi] = state[xi][yi];
+		}
+	}
+	pb->NumOfMoves = NumOfMoves;
+	pb->Parent = Parent;
+	pb->FirstChild = FirstChild;
+	pb->NextBrother = NextBrother;
+	return;
+}
